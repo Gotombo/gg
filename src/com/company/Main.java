38a13ce24e48f00911717e6a519a2fd747e07d88
gotom <ganievomar6@gmail.com>
2021-08-21 22:00:02 +0600

    nened

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,25 +8,40 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Введите цвет с большой буквы");
-		String color = sc.nextLine();
-		switch (color) {
-			case "Красный":
-				System.out.println("Вы грубый и агресивный");
-				break;
-			case "Зеленый":
-				System.out.println("НЕт ответа");
-			case "Белый":
-				System.out.println("Вы добрый и мягкий");
-				break;
-			default:
-				System.out.println("404 Error Not Found");
-				int a = 23;
+		System.out.println("Введите ваш возраст");
+		int age = sc.nextInt();
+
+		Scanner rc = new Scanner(System.in);
+		System.out.println("Введите температуру");
+		int temp = sc.nextInt();
+
+		if( age > 20 && age < 45 || temp > -20 && temp < 30 ||
+				age<20 && temp > 0 && temp < 28 || age > 45 && temp > -10 && temp < 25 ){
+			System.out.print("можно гулять");
+		}else{
+			System.out.print("Оставайся дома");
+
+		}
+		}
+
+//		switch (age) {
+	//case "Красный":
+	//			System.out.println("Вы грубый и агресивный");
+	//			break;
+	//		case "Зеленый":
+	//			System.out.println("НЕт ответа");
+	//		case "Белый":
+	//			System.out.println("Вы добрый и мягкий");
+	//			break;
+	//		default:
+	//			System.out.println("404 Error Not Found");
+	//			int a = 23;
 
 		}
 
 
-	}
-}
+
+
 
